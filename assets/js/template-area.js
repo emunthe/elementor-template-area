6de8147030981fce7895144a09f0ee54c02a5c46
Espen Munthe <mail@espenmunthe.com>
2018-09-08 18:02:09 +0200

    template 1%

--- assets/js/template-area.js
+++ assets/js/template-area.js
@@ -1,16 +1,10 @@
 (function($) {
-    var GlobalTemplateAreaHandler = function($scope, $) {
-        console.log('GlobalTemplateAreaHandler', $scope);
-    };
-
     var WidgetTemplateAreaHandler = function($scope, $) {
-        console.log($scope);
-        console.log('template-area.default');
+        console.log('template-area.default', $scope);
     };
 
     // Make sure you run this code under Elementor..
     $(window).on('elementor/frontend/init', function() {
-        elementorFrontend.hooks.addAction('frontend/element_ready/global', GlobalTemplateAreaHandler);
         elementorFrontend.hooks.addAction('frontend/element_ready/template-area.default', WidgetTemplateAreaHandler);
     });
 })(jQuery);
