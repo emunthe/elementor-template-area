c80fb0b766aed89aeb347500eb816937bc9253b8
Espen Munthe <mail@espenmunthe.com>
2018-09-08 13:21:49 +0200

    template 1%

--- assets/js/template-area.js
+++ assets/js/template-area.js
@@ -1,10 +1,10 @@
 (function($) {
-    var WidgetCustomColumnHandler = function($scope, $) {
+    var WidgetTemplateAreaHandler = function($scope, $) {
         console.log($scope);
     };
 
     // Make sure you run this code under Elementor..
     $(window).on('elementor/frontend/init', function() {
-        elementorFrontend.hooks.addAction('frontend/element_ready/template-area.default', WidgetCustomColumnHandler);
+        elementorFrontend.hooks.addAction('frontend/element_ready/template-area.default', WidgetTemplateAreaHandler);
     });
 })(jQuery);
