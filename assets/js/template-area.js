6889cd60f9cc85a2e538a4255ff23e4241c7474e
Espen Munthe <mail@espenmunthe.com>
2018-09-08 18:29:56 +0200

    template 1%

--- assets/js/template-area.js
+++ assets/js/template-area.js
@@ -1,10 +1,19 @@
 (function($) {
     var WidgetTemplateAreaHandler = function($scope, $) {
-        console.log('template-area.default', $scope);
+        console.log('template-area.default', $scope.attributes);
+
+        var thisElement = 'elementor-element-' + $scope.attributes['data-id'];
+
+        console.log(thisElement);
+        //$( "#target" ).click(function() {
+          //alert( "Handler for .click() called." );
+        //});
+
     };
 
     // Make sure you run this code under Elementor..
     $(window).on('elementor/frontend/init', function() {
         elementorFrontend.hooks.addAction('frontend/element_ready/template-area.default', WidgetTemplateAreaHandler);
     });
+
 })(jQuery);
