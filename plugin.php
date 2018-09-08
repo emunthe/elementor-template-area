8d77feb9046f04b2b103d5a4f73aec02d8927ce8
Espen Munthe <mail@espenmunthe.com>
2018-09-08 17:55:14 +0200

    template 1%

--- plugin.php
+++ plugin.php
@@ -46,7 +46,7 @@ class Plugin {
 	 * @access public
 	 */
 	public function widget_scripts() {
-		wp_register_script( 'template-area', plugins_url( '/assets/js/template-area.js', __FILE__ ), [ 'jquery' ], Elementor_Template_Area::VERSION, true );
+		wp_register_script( 'template-area', plugins_url( '/assets/js/template-area.js', __FILE__ ), [ 'jquery' ], VERSION, true );
 		wp_enqueue_script( 'template-area' );
 	}
 
