dae0729ed9f08ba370d7c5d5c0348313e4d06223
Espen Munthe <mail@espenmunthe.com>
2018-09-08 13:23:18 +0200

    template 1%

--- assets/js/template-area.js
+++ assets/js/template-area.js
@@ -1,6 +1,7 @@
 (function($) {
     var WidgetTemplateAreaHandler = function($scope, $) {
         console.log($scope);
+        console.log('here');
     };
 
     // Make sure you run this code under Elementor..
